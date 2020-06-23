2ceab400e87167a006a55a746ae1df911f4ab699
Gary Mann <g.mann@sydney.edu.au>
2020-06-23 16:41:40 +1000

    Changed TraditionStaticAssignment to report impossible routes.  Updating of the testing document

--- src/main/java/org/planit/tntp/output/formatter/CSVOutputFormatter.java
+++ src/main/java/org/planit/tntp/output/formatter/CSVOutputFormatter.java
@@ -81,7 +81,8 @@ public class CSVOutputFormatter extends CsvFileOutputFormatter implements CsvTex
 		try {
 			for (final Mode mode : modes) {
 				for (final LinkSegment linkSegment : linkOutputTypeAdapter.getPhysicalLinkSegments()) {
-					if (outputTypeConfiguration.isPersistZeroFlow() || linkOutputTypeAdapter.isFlowPositive(linkSegment, mode)) {						final List<Object> rowValues = new ArrayList<Object>();
+					if (outputConfiguration.isPersistZeroFlow() || linkOutputTypeAdapter.isFlowPositive(linkSegment, mode)) {
+					  final List<Object> rowValues = new ArrayList<Object>();
 						for (final BaseOutputProperty outputProperty : outputProperties) {
               rowValues.add(OutputUtils.formatObject(linkOutputTypeAdapter.getLinkOutputPropertyValue(outputProperty.getOutputProperty(), linkSegment, mode, timePeriod, outputTimeUnit.getMultiplier())));
  						}
